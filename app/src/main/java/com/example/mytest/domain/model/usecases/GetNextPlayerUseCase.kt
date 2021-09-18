eeb76462ade80d81abb0b1f908e35e3bc918b015
Jose Lopez <jmlb0003@gmail.com>
2021-09-18 17:23:41 +0200

    Add GetNextPlayerUseCase

---
+++ app/src/main/java/com/example/mytest/domain/model/usecases/GetNextPlayerUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.mytest.domain.model.usecases
+
+import com.example.mytest.domain.model.Clear
+import com.example.mytest.domain.model.OPlayer
+import com.example.mytest.domain.model.OSelected
+import com.example.mytest.domain.model.Player
+import com.example.mytest.domain.model.XPlayer
+import com.example.mytest.domain.model.XSelected
+import com.example.mytest.domain.model.repositories.BoardRepository
+import kotlinx.coroutines.flow.lastOrNull
+
+class GetNextPlayerUseCase(
+    private val boardRepository: BoardRepository
+) {
+    suspend operator fun invoke(): Player =
+        boardRepository.getBoard().lastOrNull()?.let { currentBoard ->
+            when (currentBoard.cells.last().state) {
+                XSelected -> OPlayer
+                Clear,
+                OSelected -> XPlayer
+            }
+        } ?: XPlayer
+}
