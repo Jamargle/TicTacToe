04c8b49519a724364978ed62fa2c00b9722d34ac
Jose Lopez <jmlb0003@gmail.com>
2021-09-20 18:09:37 +0200

    Add unit tests for the logic needed in the implementation of the board repository

---
+++ app/src/test/java/com/example/mytest/data/OnMemoryBoardRepositoryTest.kt
@@ -0,0 +1,66 @@
+package com.example.mytest.data
+
+import com.example.mytest.domain.model.Cell
+import com.example.mytest.domain.model.Clear
+import com.example.mytest.domain.model.OPlayer
+import com.example.mytest.domain.model.OSelected
+import com.example.mytest.domain.model.XPlayer
+import com.example.mytest.domain.model.XSelected
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class OnMemoryBoardRepositoryTest {
+
+    private val repository = OnMemoryBoardRepository()
+
+    @Test
+    fun `starts with clear board`() = runBlockingTest {
+        val board = repository.getBoard().first()
+        assertEquals(9, board.cells.filter { it.state == Clear }.size)
+
+        assertNull(board.cells.find { it.state == XSelected })
+        assertNull(board.cells.find { it.state == OSelected })
+    }
+
+    @Test
+    fun `updateCellSelection returns failure when trying to update a cell that was already selected`() =
+        runBlockingTest {
+            val resultMove1 = repository.updateCellSelection(Cell(0, 0), XPlayer)
+            val resultMove2 = repository.updateCellSelection(Cell(0, 0), XPlayer)
+            val resultMove3 = repository.updateCellSelection(Cell(0, 0), OPlayer)
+
+            assertTrue(resultMove1.isSuccess)
+            assertTrue(resultMove2.isFailure)
+            assertTrue(resultMove3.isFailure)
+        }
+
+    @Test
+    fun `updateCellSelection updates board flow when board changes`() = runBlockingTest {
+        val initialBoard = repository.getBoard().first()
+        repository.updateCellSelection(Cell(0, 0), XPlayer)
+
+        val boardAfterUpdate = repository.getBoard().first()
+
+        with(initialBoard) {
+            assertEquals(9, cells.filter { it.state == Clear }.size)
+            assertEquals(0, cells.filter { it.state == XSelected }.size)
+        }
+        with(boardAfterUpdate) {
+            assertEquals(8, cells.filter { it.state == Clear }.size)
+            assertEquals(1, cells.filter { it.state == XSelected }.size)
+        }
+    }
+
+    @Test
+    fun `updateCellSelection returns success if selection changed`() = runBlockingTest {
+        val result = repository.updateCellSelection(Cell(0, 0), XPlayer)
+        assertTrue(result.isSuccess)
+    }
+
+}
