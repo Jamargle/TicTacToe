f1966021b4ce5acc3cbbc40ca9a8b09a6764ea2a
Jose Lopez <jmlb0003@gmail.com>
2021-09-20 21:36:09 +0200

    Update implementation to make the BoardViewModel tests pass

--- app/src/main/java/com/example/mytest/presentation/board/BoardViewModel.kt
+++ app/src/main/java/com/example/mytest/presentation/board/BoardViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.mytest.presentation.board
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mytest.domain.model.Board
 import com.example.mytest.domain.model.Cell
-import com.example.mytest.domain.model.Clear
+import com.example.mytest.domain.model.OPlayer
+import com.example.mytest.domain.model.Player
+import com.example.mytest.domain.model.XPlayer
 import com.example.mytest.domain.usecases.CheckGameStateUseCase
 import com.example.mytest.domain.usecases.GetBoardStateUseCase
 import com.example.mytest.domain.usecases.GetNextPlayerUseCase
@@ -15,40 +16,45 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class BoardViewModel(
+    private val viewState: BoardViewState,
     private val getBoardState: GetBoardStateUseCase,
     private val checkGameState: CheckGameStateUseCase,
     private val getNextPlayer: GetNextPlayerUseCase,
     private val selectCell: SelectCellUseCase
 ) : ViewModel() {
 
-    private val boardState: MutableLiveData<Board> = MutableLiveData()
-
     init {
+        viewState.showLoading()
         viewModelScope.launch {
             getBoardState.invoke().collect {
-                boardState.postValue(it)
+                viewState.updateBoard(it)
+                viewState.updateTurn(getNextTurn())
             }
         }
     }
 
-    fun getBoardState(): LiveData<Board> = boardState
+    fun getBoardState(): LiveData<Board> = viewState.boardState
 
     fun onCellClicked(cell: Cell) {
         viewModelScope.launch {
-            val nextPlayer = getNextPlayer()
-            if (cell.state != Clear) {
-                return@launch
-            }
-            val result = selectCell(cell, nextPlayer)
-            result.fold(
+            val currentPlayer = getNextPlayer()
+
+            selectCell(cell, currentPlayer).fold(
                 onFailure = {
                     println("Error selecting the cell $cell")
                 },
                 onSuccess = {
-                    println("Cell $cell marked as selected by $nextPlayer")
+                    viewState.updateTurn(getNextTurn(currentPlayer))
                 }
             )
         }
     }
+
+    private suspend fun getNextTurn(currentPlayer: Player? = null) =
+        when (currentPlayer) {
+            is XPlayer -> OPlayer
+            is OPlayer -> XPlayer
+            else -> getNextPlayer()
+        }
 
 }
