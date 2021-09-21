bc863184749400fafa717f672e6141716228a1bb
Jose Lopez <jmlb0003@gmail.com>
2021-09-21 22:42:04 +0200

    Add ViewBinding

--- app/src/main/java/com/example/mytest/app/features/board/BoardFragment.kt
+++ app/src/main/java/com/example/mytest/app/features/board/BoardFragment.kt
@@ -1,15 +1,21 @@
 package com.example.mytest.app.features.board
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mytest.R
 import com.example.mytest.app.CustomApplication
 import com.example.mytest.app.di.BoardViewModelFactory
+import com.example.mytest.databinding.FragmentBoardBinding
 import com.example.mytest.presentation.board.BoardViewModel
 import javax.inject.Inject
 
 class BoardFragment : Fragment(R.layout.fragment_board) {
+
+    private var _binding: FragmentBoardBinding? = null
+    private val binding get() = checkNotNull(_binding)
 
     @Inject
     lateinit var boardViewModelFactory: BoardViewModelFactory
@@ -19,6 +25,16 @@ class BoardFragment : Fragment(R.layout.fragment_board) {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         initApplicationComponent()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentBoardBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 
     private fun initApplicationComponent() {
