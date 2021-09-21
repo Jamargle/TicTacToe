f90f6137ae9f464b72267524164c480b6168d848
Jose Lopez <jmlb0003@gmail.com>
2021-09-21 22:20:37 +0200

    Update in the UI tests before implementing logic to update cell views on click

--- app/src/androidTest/java/com/example/mytest/app/features/board/BoardFragmentTest.kt
+++ app/src/androidTest/java/com/example/mytest/app/features/board/BoardFragmentTest.kt
@@ -3,7 +3,9 @@ package com.example.mytest.app.features.board
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.mytest.R
 import com.example.mytest.app.MainActivity
+import com.example.mytest.domain.model.OPlayer
 import com.example.mytest.domain.model.XPlayer
 import org.junit.Rule
 import org.junit.Test
@@ -30,4 +32,47 @@ class BoardFragmentTest {
             hasClearBoard()
         }
     }
+
+    @Test
+    fun onSelectCellsUpdatesThemForEachCurrentPlayerUntilCompletion() {
+        activityScenarioTestRule.scenario.run {
+            moveToState(Lifecycle.State.RESUMED)
+
+            onTurnView {
+                hasNextTurnWith(XPlayer)
+            }
+            onBoardView {
+                hasClearBoard()
+            }
+        }
+
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_9, XPlayer) }
+
+        onTurnView { hasNextTurnWith(OPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_8, OPlayer) }
+
+        onTurnView { hasNextTurnWith(XPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_7, XPlayer) }
+
+        onTurnView { hasNextTurnWith(OPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_6, OPlayer) }
+
+        onTurnView { hasNextTurnWith(XPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_5, XPlayer) }
+
+        onTurnView { hasNextTurnWith(OPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_3, OPlayer) }
+
+        onTurnView { hasNextTurnWith(XPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_2, XPlayer) }
+
+        onTurnView { hasNextTurnWith(OPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_1, OPlayer) }
+
+        onTurnView { hasNextTurnWith(XPlayer) }
+        onBoardView { onClickOnCellSelectsItForPlayer(R.id.cell_4, XPlayer) }
+    }
+
+    // TODO Add check for completion
+
 }
--- app/src/androidTest/java/com/example/mytest/app/features/board/BoardRobot.kt
+++ app/src/androidTest/java/com/example/mytest/app/features/board/BoardRobot.kt
@@ -2,8 +2,11 @@ package com.example.mytest.app.features.board
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withTagValue
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.example.mytest.R
 import com.example.mytest.domain.model.OPlayer
@@ -13,6 +16,7 @@ import com.example.mytest.utils.isDisplayed
 import com.example.mytest.utils.withCompoundDrawable
 import com.example.mytest.utils.withText
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.not
 
 inline fun onTurnView(block: BoardRobot.() -> Unit) = BoardRobot().apply(block)
@@ -59,6 +63,12 @@ class BoardRobot {
         }
     }
 
+    fun onClickOnCellSelectsItForPlayer(cellId: Int, player: Player) {
+        onView(withId(cellId)).perform(click())
+
+        onView(withParent(withId(cellId))).isSelectedForPlayer(player)
+    }
+
     private fun ViewInteraction.isNotSelected() {
         check(
             matches(
@@ -68,5 +78,13 @@ class BoardRobot {
                 )
             )
         )
+    }
+
+    private fun ViewInteraction.isSelectedForPlayer(player: Player) {
+        val playerSelectionDrawable = when (player) {
+            XPlayer -> R.drawable.ic_x
+            OPlayer -> R.drawable.ic_o
+        }
+        check(matches(withTagValue(equalTo(playerSelectionDrawable))))
     }
 }
