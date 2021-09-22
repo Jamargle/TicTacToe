ae3d8a08d7861854f496e930c68003555db4d81d
Jose Lopez <jmlb0003@gmail.com>
2021-09-22 21:59:43 +0200

    Update UnselectCellUseCase tests as there are not suspend functions involved

--- app/src/test/java/com/example/mytest/domain/usecases/UnselectCellUseCaseTest.kt
+++ app/src/test/java/com/example/mytest/domain/usecases/UnselectCellUseCaseTest.kt
@@ -7,39 +7,34 @@ import com.example.mytest.domain.repositories.BoardRepository
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
-@ExperimentalCoroutinesApi
 class UnselectCellUseCaseTest {
 
     private val boardRepository = mockk<BoardRepository>()
     private val unSelectCellUseCase = UnselectCellUseCase(boardRepository)
 
     @Test
-    fun `returns success if given cell is already unselected`() = runBlockingTest {
+    fun `returns success if given cell is already unselected`() {
         assertTrue(unSelectCellUseCase(Cell(0, 0, Clear)).isSuccess)
     }
 
     @Test
-    fun `returns failure if cell is selected but unSelection is not fine from boardRepository`() =
-        runBlockingTest {
-            val givenCell = Cell(0, 0, XSelected)
-            coEvery { boardRepository.clearCellSelection(givenCell) } returns Result.failure(
-                Throwable("")
-            )
-            assertTrue(unSelectCellUseCase(givenCell).isFailure)
-        }
+    fun `returns failure if cell is selected but unSelection is not fine from boardRepository`() {
+        val givenCell = Cell(0, 0, XSelected)
+        coEvery { boardRepository.clearCellSelection(givenCell) } returns Result.failure(
+            Throwable("")
+        )
+        assertTrue(unSelectCellUseCase(givenCell).isFailure)
+    }
 
     @Test
-    fun `returns success if cell is selected and unSelection is fine from boardRepository`() =
-        runBlockingTest {
-            val givenCell = Cell(0, 0, XSelected)
-            every { boardRepository.clearCellSelection(givenCell) } returns
-                    Result.success(Unit)
+    fun `returns success if cell is selected and unSelection is fine from boardRepository`() {
+        val givenCell = Cell(0, 0, XSelected)
+        every { boardRepository.clearCellSelection(givenCell) } returns
+                Result.success(Unit)
 
-            assertTrue(unSelectCellUseCase(Cell(0, 0, XSelected)).isSuccess)
-        }
+        assertTrue(unSelectCellUseCase(Cell(0, 0, XSelected)).isSuccess)
+    }
 }
