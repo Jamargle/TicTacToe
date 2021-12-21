1c22dfd1e83fbb3676d6fe620313badd11ac1919
Jose Lopez <jmlb0003@gmail.com>
2021-12-21 20:51:26 +0100

    Update repository to move the business logic to the usecases

--- app/src/main/java/com/example/tictactoe/domain/repositories/BoardRepository.kt
+++ app/src/main/java/com/example/tictactoe/domain/repositories/BoardRepository.kt
@@ -2,14 +2,14 @@ package com.example.tictactoe.domain.repositories
 
 import com.example.tictactoe.domain.model.Board
 import com.example.tictactoe.domain.model.Cell
-import com.example.tictactoe.domain.model.Player
+import com.example.tictactoe.domain.model.CellState
 import kotlinx.coroutines.flow.StateFlow
 
 interface BoardRepository {
 
     suspend fun getBoard(): StateFlow<Board>
 
-    fun updateCellSelection(cell: Cell, player: Player): Result<Unit>
+    fun updateCellSelection(cell: Cell, selection: CellState): Result<Unit>
 
     fun clearCellSelection(cell: Cell): Result<Unit>
 
